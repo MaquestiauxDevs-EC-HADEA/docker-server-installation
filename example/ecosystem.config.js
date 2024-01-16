81cae7307188f766e8f1f3560c997970fa4fc330
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2024-01-16 11:01:20 +0100

    Use container ip as dockerised

--- example/ecosystem.config.js
+++ example/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         HTTP_PORT: 3000,
         HTTP_HOST: "localhost",
         NODE_ENV: "development",
-        DATABASE_HOST: "localhost", // Use container ip if dockerised : 172.17.0.4
+        DATABASE_HOST: "172.17.0.4", // Use container ip as dockerised
         DATABASE_PORT: 1521, // Oracle Container port on Portainer
         DATABASE_SID: "ORCLCDB",
         DATABASE_SERVICE_NAME: "ORCLCDB",
