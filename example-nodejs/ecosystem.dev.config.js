743b584400d2bee8224d07c6f4cb29053f413f70
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2024-01-30 17:27:24 +0100

    Update local IP

--- example-nodejs/ecosystem.dev.config.js
+++ example-nodejs/ecosystem.dev.config.js
@@ -8,7 +8,7 @@ module.exports = {
         HTTP_PORT: 3000,
         HTTP_HOST: "localhost",
         NODE_ENV: "development",
-        DATABASE_HOST: "172.17.0.2", // Use container ip as dockerised (!! Can change with portainer restart)
+        DATABASE_HOST: "172.17.0.4", // Use container ip as dockerised (!! Can change with portainer restart)
         DATABASE_PORT: 1521, // Oracle Container port on Portainer
         DATABASE_SID: "ORCLCDB",
         DATABASE_SERVICE_NAME: "ORCLCDB",
