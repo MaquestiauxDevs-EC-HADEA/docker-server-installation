9523a717187d99f12280dd80796b3e1f3f7f9307
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2024-02-02 08:52:50 +0100

    Move Envs from stack to supervisord conf file

--- example-php/04-generate-project-docker-image.sh
+++ example-php/04-generate-project-docker-image.sh
@@ -2,6 +2,7 @@
 echo "First arg: $1"
 if [ "$1" == "dev" ]; then
     echo "Running in development mode"
+    yes | cp configs/supervisor/supervisord.dev.conf configs/supervisor/supervisord.conf
     docker image rm example-php:dev
     docker build -t example-php:dev .
     docker save example-php:dev > example-php-dev.tar
@@ -15,6 +16,7 @@ fi
 
 if [ "$1" == "prod" ]; then
     echo "Running in production mode"
+    yes | cp configs/supervisor/supervisord.prod.conf configs/supervisor/supervisord.conf
     docker image rm example-php:latest
     docker build -t example-php:latest .
     docker save example-php:latest > example-php.tar
